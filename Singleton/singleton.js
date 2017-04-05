a3844ddc448a9ffa0dfd5df25a673b9ebe049159
Chainn <578579786@qq.com>
2017-04-05 22:56:38 +0800

    Add singleton

---
+++ Singleton/singleton.js
@@ -0,0 +1,20 @@
+var getSingle = function( fn ){
+  var result;
+  return function(){
+    return result || ( result = fn .apply(this, arguments ) );
+  }
+};
+
+//usage
+var createLoginLayer = function(){
+  var div = document.createElement( 'div' );
+  div.innerHTML = '我是登录浮窗';
+  div.style.display = 'none';
+  document.body.appendChild( div );
+  return div;
+};
+var createSingleLoginLayer = getSingle( createLoginLayer );
+document.getElementById( 'loginBtn' ).onclick = function(){
+  var loginLayer = createSingleLoginLayer();
+  loginLayer.style.display = 'block';
+};
